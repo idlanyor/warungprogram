0b630c73e486e4fafe59e5f6a47287ffcf1b4a2f
idlanyor <prasbhara0604@gmail.com>
2024-04-15 16:14:59 +0700

    ganti ke react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./Components/Card"
+import Navbar from "./Components/Navbar"
+import Section from "./Components/Section"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Card />
+      <Section />
+    </>
+  )
+}
+
+export default App
