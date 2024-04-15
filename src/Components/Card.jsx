a6ce246520107d3fbc03e0e30d20c73d56692b1f
idlanyor <prasbhara0604@gmail.com>
2024-04-15 19:05:47 +0700

    replace animate.css from cdn to package btw gambar masih burik yak

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,18 @@
 export default function Card() {
     return (
-        <div className="card relative max-w-xl h-[450]">
-            <div className="card-text animate__animated animate__fadeInUp">
+        <div className="card flex justify-center items-center relative max-w-screen-2xl h-screen">
+            <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center" style={{
+                backgroundImage: "url(https://i.ibb.co/7yQhcDG/wp.jpg)",
+                filter: "brightness(0.4)"
+            }}></div>
+            <div className="left-1/2 space-y-5 px-8 transform -translate-x-1/2 -translate-y-1/2 text-center text-white w-full z-10 animate__animated animate__fadeInUp">
                 <h1 className="text-3xl font-extrabold">Warung Program</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id aliquam leo. Morbi eleifend mauris eget
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus cum aspernatur mollitia nostrum praesentium veniam tempore molestias quae optio nisi, quo nam ducimus amet ratione corporis accusamus vitae fuga maiores accusantium eos officia eveniet dolor distinctio. Quae velit temporibus error officia laudantium facilis, expedita asperiores excepturi delectus ad! Asperiores, eaque.
                     lorem vestibulum, nec sodales nisi ultricies.</p>
                 <button type="button"
                     className="text-white bg-[#00008B] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2">
                     Learn More&emsp;
-                    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                    <svg className="w-6 h-6 text-white" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                             d="M1 5h12m0 0L9 1m4 4L9 9" />
--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -8,7 +8,7 @@ export default function Section() {
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"
                     alt="dashboard image" />
                 <div className="mt-4 md:mt-0">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's create more tools
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Lets create more tools
                         and ideas that brings us together.</h2>
                     <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends
                         and communities of people who share your interests. Connecting with your friends and family as well as
